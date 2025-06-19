bb3ce02dfa63f75bfc0865966fd2fd9a733a8103
Sarah Q <qiaosarah8@gmail.com>
2025-06-19 14:48:53 -0400

    Initial Commit

---
+++ GaitAnalysisApp/GaitAnalysisAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaitAnalysisAppApp.swift
+//  GaitAnalysisApp
+//
+//  Created by Sarah Qiao on 2025-06-19.
+//
+
+import SwiftUI
+
+@main
+struct GaitAnalysisAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
