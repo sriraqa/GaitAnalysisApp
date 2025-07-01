f71e100ddc22f31a33d6ad089bac339b6057e08b
Sarah Q <qiaosarah8@gmail.com>
2025-07-01 16:36:11 -0400

    update for embedded code

--- GaitAnalysisApp/BluetoothManager.swift
+++ GaitAnalysisApp/BluetoothManager.swift
@@ -6,22 +6,177 @@
 //
 
 import Foundation
+import CoreBluetooth
 
-class BluetoothManager: NSObject, ObservableObject {
-    @Published var forceLevel: Int = 0;
-    
-    private var bluetoothIO: BluetoothIO!
-    
-    override init() {
-        super.init()
-        bluetoothIO = BluetoothIO(serviceUUID: "42f66074-73ac-44ba-b655-483c6f673e33", delegate: self)
-    }
+struct Peripheral: Identifiable {
+    let id: UUID
+    let name: String
+    let rssi: Int
+    let advertisementServiceUUIDs: [String]?
+    let peripheral: CBPeripheral
 }
 
-extension BluetoothManager: BluetoothIODelegate {
-    func bluetoothIO(bluetoothIO: BluetoothIO, didReceiveValue value: Int8) {
-        DispatchQueue.main.async {
-            self.forceLevel = Int(value)
+struct ForceData: Decodable, Equatable {
+    let heel: Int
+    let big_toe: Int
+    let arch: Int
+    let ball: Int
+    let sole: Int
+}
+
+// BLEManager class conforms to observable object for SwiftUI, CBCentralManagerDelegate and CBPeripheralDelegate for managing BLE connections.
+class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeripheralDelegate {
+
+    // Constants specific to the ESP32 BLE service and characteristic UUIDs.
+    enum ESP32Constants {
+        static let serviceUUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
+        static let characteristicUUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
+    }
+
+    // Central manager to handle BLE operations.
+    var centralManager: CBCentralManager!
+    // Reference to the connected ESP32 peripheral.
+    var esp32Peripheral: CBPeripheral?
+    // Reference to the characteristic through which communication happens.
+    var esp32Characteristic: CBCharacteristic?
+
+    @Published var peripherals: [Peripheral] = []
+    @Published var isConnected = false // Indicates if the app is connected to a peripheral.
+    @Published var forceLevel: ForceData = ForceData(heel: 0, big_toe: 0, arch: 0, ball: 0, sole: 0) // numbers that indicates the read values
+
+    override init() {
+        super.init()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+
+    // Function to send a string value to the ESP32.
+    func sendTextValue(_ text: String) {
+        let data = Data(text.utf8)
+        if let myCharacteristic = esp32Characteristic {
+            esp32Peripheral?.writeValue(data, for: myCharacteristic, type: .withResponse)
         }
+    }
+
+    // Function to initiate a connection to a chosen peripheral.
+    func connectPeripheral(peripheral: Peripheral) {
+        guard let foundPeripheral = peripherals.first(where: { $0.id == peripheral.id })?.peripheral else { return }
+        esp32Peripheral = foundPeripheral
+        esp32Peripheral?.delegate = self
+        centralManager.connect(foundPeripheral, options: nil)
+    }
+
+    // Function to disconnect from the current peripheral.
+    func disconnectFromPeripheral() {
+        if let peripheral = esp32Peripheral {
+            centralManager.cancelPeripheralConnection(peripheral)
+        }
+    }
+
+    func refreshDevices() {
+        if centralManager.state == .poweredOn {
+            peripherals.removeAll()
+            centralManager.scanForPeripherals(withServices: nil, options: nil)
+            print("Started scanning")
+        } else {
+            print("Bluetooth is not powered on")
+        }
+    }
+
+    // MARK: - CBCentralManagerDelegate Methods
+
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        refreshDevices()
+    }
+
+    // Called when a peripheral is discovered during scanning.
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String: Any], rssi RSSI: NSNumber) {
+        if peripheral.name?.contains("ESP32") ?? false {
+            let adsServiceUUIDs = (advertisementData[CBAdvertisementDataServiceUUIDsKey] as? [CBUUID])?.compactMap({ data in
+                data.uuidString
+            })
+
+            let newPeripheral = Peripheral(id: peripheral.identifier,
+                                           name: peripheral.name ?? "Unknown",
+                                           rssi: RSSI.intValue,
+                                           advertisementServiceUUIDs: adsServiceUUIDs,
+                                           peripheral: peripheral)
+            if !peripherals.contains(where: { $0.id == newPeripheral.id }) {
+                peripherals.append(newPeripheral)
+            }
+        }
+    }
+
+    // Called when a connection is successfully established with a peripheral.
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        isConnected = true
+        print("Connected to \(peripheral.name ?? "Unknown")")
+        esp32Peripheral?.discoverServices([CBUUID(string: ESP32Constants.serviceUUID)])
+        centralManager.stopScan()
+    }
+
+    // Called when the peripheral is disconnected.
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        isConnected = false
+        esp32Peripheral = nil
+        esp32Characteristic = nil
+        peripherals.removeAll()
+        refreshDevices()
+    }
+
+    // MARK: - CBPeripheralDelegate Methods
+
+    // Called when services of a peripheral are discovered.
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        if let services = peripheral.services {
+            for service in services {
+                // Discover characteristics for the service with the specified UUID
+                print("Discovered service: \(service.uuid)")
+                peripheral.discoverCharacteristics(nil, for: service)
+            }
+        }
+    }
+
+    // Called when characteristics for a service are discovered.
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        if let characteristics = service.characteristics {
+            for characteristic in characteristics {
+                if characteristic.properties.contains(.notify) {
+                    esp32Characteristic = characteristic
+                    print("Properties: \(characteristic.properties)")
+
+                    peripheral.setNotifyValue(true, for: characteristic)
+                }
+            }
+        }
+    }
+
+    // Called when the value of a characteristic is updated.
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        if let error = error {
+            print("Error: \(error.localizedDescription)")
+            return
+        }
+
+        if characteristic == esp32Characteristic {
+            if let data = characteristic.value {
+                print("Force Levels: \(data)")
+                do {
+                    let decoded = try JSONDecoder().decode(ForceData.self, from: data)
+                    forceLevel = decoded
+                    print("Force Levels: \(forceLevel)")
+                } catch {
+                    print(error)
+                }
+            }
+        }
+    }
+
+    // Called when a value is successfully written to a characteristic.
+    func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: Error?) {
+        if let error = error {
+            print("Error in sending data: \(error)")
+            return
+        }
+        print("Data has been successfully sent and processed.")
     }
 }
--- GaitAnalysisApp/ContentView.swift
+++ GaitAnalysisApp/ContentView.swift
@@ -6,22 +6,119 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
-    @StateObject private var bluetoothManager = BluetoothManager()
+    @ObservedObject var bluetoothManager = BLEManager()
+    @State var mirror: [(String, Int)]? = nil
     
     var body: some View {
-        VStack(spacing: 8) {
-            Text("Force Sensor Value:")
-            Text("\(bluetoothManager.forceLevel)")
-                .foregroundColor(bluetoothManager.forceLevel < 10 ? .green : .red)
+        VStack(alignment: .center, spacing: 16) {
+            if bluetoothManager.peripherals.isEmpty {
+                ProgressView("Searching for ESP32 Module")
+                    .progressViewStyle(.circular)
+            } else if bluetoothManager.isConnected {
+                Text("Force Sensor Values:")
+                    .font(.system(size: 24))
+                    .bold()
+                VStack(spacing: 8) {
+                    if let values = mirror {
+                        ForEach(values, id: \.0) { label, number in
+                            Text("\(label): \(number)")
+                                .foregroundColor(getColor(for: number))
+                        }
+                    }
+                }
+                Spacer()
+                List {
+                    Button("Disconnect") {
+                        bluetoothManager.disconnectFromPeripheral()
+                    }
+                    .foregroundStyle(.red)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                }
+                .onAppear {
+                    mirror = [
+                        ("Big Toe", 0),
+                        ("Ball", 0),
+                        ("Sole", 0),
+                        ("Arch", 0),
+                        ("Heel", 0)
+                    ]
+                }
+            } else {
+                List(bluetoothManager.peripherals) { peripheral in
+                    device(peripheral)
+                }
+                .refreshable {
+                    bluetoothManager.refreshDevices()
+                }
+            }
         }
+        .frame(maxHeight: .infinity)
         .padding()
+        .onChange(of: bluetoothManager.forceLevel) { forceData in
+            mirror = [
+                ("Big Toe", forceData.big_toe),
+                ("Ball", forceData.ball),
+                ("Sole", forceData.sole),
+                ("Arch", forceData.arch),
+                ("Heel", forceData.heel)
+            ]
+        }
     }
-}
+    
+    func device(_ peripheral: Peripheral) -> some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Text(peripheral.name)
+                Spacer()
+                Button(action: {
+                    bluetoothManager.connectPeripheral(peripheral: peripheral)
+                }) {
+                    Text("Connect")
+                }
+                .buttonStyle(.borderedProminent)
+            }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+            Divider()
+
+            VStack(alignment: .leading) {
+                Group {
+                    Text("""
+                          Device UUID:
+                          \(peripheral.id.uuidString)
+                          """)
+                    .padding([.bottom], 10)
+
+                    if let adsServiceUUIDs = peripheral.advertisementServiceUUIDs {
+                        Text("Advertisement Service UUIDs:")
+                        ForEach(adsServiceUUIDs, id: \.self) { uuid in
+                            Text(uuid)
+                        }
+                    }
+
+                    HStack {
+                        Image(systemName: "chart.bar.fill")
+                        Text("\(peripheral.rssi) dBm")
+                    }
+                    .padding([.top], 10)
+                }
+                .font(.footnote)
+            }
+        }
+    }
+    
+    func getColor(for value: Int) -> Color {
+        switch value {
+        case ..<50:
+            return .green
+        case 50..<150:
+            return .yellow
+        case 150..<300:
+            return .orange
+        default:
+            return .red
+        }
     }
 }
